3478399171b06dec3879bde0558d1f744ba6061d
xiaopeizi2 <117981862+xiaopeizi2@users.noreply.github.com>
2022-11-11 22:42:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '裴静茹宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '	osAJk6sRgGqvCQMilb5mbd8vNkog	',
+      id: 'osAJk6sRgGqvCQMilb5mbd8vNkog	',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'eDVt0uv6zxlHjXJIooZ5bFOTQo5ivHp2c2RUkH_l7mk',
+      useTemplateId: 'eDVt0uv6zxlHjXJIooZ5bFOTQo5ivHp2c2RUkH_l7mk	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-12',
       festivals: [
@@ -55,7 +55,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: ' gh_1efb478a86ec',
+      id: ' osAJk6tGWhyVgF3or9wa8kzvU9iI	',
     }
   ],
 
